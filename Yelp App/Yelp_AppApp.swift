7ce2f5ad871a995c08249d61ad6e4eccd8986a06
abood <abdelrahman.mohammad02@gmail.com>
2022-08-08 12:50:03 -0400

    Initial Commit

---
+++ Yelp App/Yelp_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_AppApp.swift
+//  Yelp App
+//
+//  Created by Abdelrahman Mohammad on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
