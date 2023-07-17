ba047fefc93e8057a48418873312134afa76df61
Abdelrahman Mohammad <57917560+aboodmo@users.noreply.github.com>
2023-07-16 20:07:47 -0400

    - made view/Model/ViewModel - started contentview
    - added user location
    - implemented Yelp API

--- Yelp App/ViewModels/ContentModel.swift
+++ Yelp App/ViewModels/ContentModel.swift
@@ -42,6 +42,84 @@ class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // Gives us Location of user
+        
+        let userlocation = locations.first
+        if userlocation != nil{
+            
+            locationManager.startUpdatingLocation()
+            
+            getBuisnesses(category: "resturants", location: userlocation!)
+        }
+    }
     
+    func getBuisnesses(category: String, location: CLLocation){
+        
+        // Create URL
+        
+        
+        var urlComponents = URLComponents(string: "https://api.yelp.com/v3/businesses/search")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "latitude", value: String(location.coordinate.latitude)),
+            URLQueryItem(name: "longitude", value: String(location.coordinate.longitude)),
+            URLQueryItem(name: "categories", value: category),
+            URLQueryItem(name: "limit", value: "6")
+        ]
+        
+        let url = urlComponents?.url
+        
+        if let url = url {
+            // Create URL Request
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.addValue("Bearer Ckeb7zKUslUk99eDlocPIiaIR-eAu-2j-2NhNTirBkRgMvQ-NFHiNarl6JDZ9L_zCqX1y-6r6wbckN09ZSrKgmj42FemiAZpuuKYWD0PhVvk7-RcDh0cgbfqxoC0ZHYx", forHTTPHeaderField: "Authorization")
+            
+            // Get URLSession
+            let session = URLSession.shared
+            
+            
+            // Create Data Task
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                if error == nil {
+                    print(response)
+                }
+            }
+            
+            // Start Data Task
+            dataTask.resume()
+            
+        }
+        
+        
+        
+//        let urlString = "s"
+//
+//
+//        let headers = ["accept": "application/json"]
+//
+//        let request = NSMutableURLRequest(url: NSURL(string: "https://api.yelp.com/v3/businesses/search")! as URL,
+//                                                cachePolicy: .useProtocolCachePolicy,
+//                                            timeoutInterval: 10.0)
+//        request.httpMethod = "GET"
+//        request.allHTTPHeaderFields = headers
+//
+//        let session = URLSession.shared
+//        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+//          if (error != nil) {
+//            print(error as Any)
+//          } else {
+//            let httpResponse = response as? HTTPURLResponse
+//            print(httpResponse)
+//          }
+//        })
+//
+//        dataTask.resume()
+        
+        // Create URL Request
+        
+        // Get URLSession
+        
+        // Create Data Task
+        
+        // Start Data Task
     }
 }
